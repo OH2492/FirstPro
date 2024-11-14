22983d260cef2fa632356f763c8f0ae0a7d3c42e
Omri <omri8262@gmail.com>
2024-11-14 14:51:09 +0200

    new

---
+++ Exercise1.js
@@ -0,0 +1,15 @@
+// Exercise 1
+let Name = "Omri Hober";
+var Name1 = "Omri Hober";
+const Name2 = "Omri Hober";
+console.log(Name, Name1, Name2);
+
+// Exercise 2
+let Age = 31;
+console.log(Age);
+Age = 32;
+console.log(Age);
+
+// Exercise 3
+const BirthDate = "24.12.92";
+console.log(BirthDate);
